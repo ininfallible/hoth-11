230e21c9cb8bd72b6ed5c3effcf9cb88356d9586
kennedy kang <kennedyekang@gmail.com>
2024-03-03 15:04:54 -0800

    formdata stuff

--- hoth-11-frontend/src/routes/+page.svelte
+++ hoth-11-frontend/src/routes/+page.svelte
@@ -1,19 +1,36 @@
 
 <script>
-    import {major} from './stores.js';
-    let mbti1 = [];
-    let mbti2 = [];
-    let mbti3 = [];
-    let mbti4 = [];
+    import {major, formData,} from './stores.js';
+    var mbti1 = "";
+    var mbti2 = "";
+    var mbti3 = "";
+    var mbti4 = "";
 
 	const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+
+    function onFormSubmit()
+    {
+        console.log("submitting form to store");
+        formData.update((_)=> {
+            return {
+                major: major,
+                mbti1: mbti1,
+                mbti2: mbti2,
+                mbti3: mbti3,
+                mbti4: mbti4
+            }
+        });
+    }
 </script>
+
+<button on:click={onFormSubmit}>Submit</button>
 
 <h1>What’s your major (ie. computer science)?</h1>
 <input bind:value = {$major} />
 
 <h2>Are you more introverted or extroverted?</h2>
 
+<form method="POST">
 {#each ['introverted' , 'extroverted'] as mbti1}
 	<label>
 		<input
@@ -25,9 +42,11 @@
         {mbti1}  
 	</label>
 {/each}
+</form>
 
 <h2>Do you tend to focus more on what’s in front of you or on what the future might hold?</h2>
 
+<form method="POST">
 {#each ['what’s right in front of me', 'what the future might hold'] as mbti2}
 	<label>
 		<input
@@ -39,9 +58,11 @@
 		{mbti2}
 	</label>
 {/each}
+</form>
 
 <h2>Are you more guided by your feelings or by logic?</h2>
 
+<form method="POST">
 {#each ['feelings', 'logic'] as mbti3}
 	<label>
 		<input
@@ -53,9 +74,11 @@
 		{mbti3}
 	</label>
 {/each}
+</form>
 
 <h2>Do you prefer planning things out or just going with the flow?</h2>
 
+<form method="POST">
 {#each ['planning things out', 'just going with the flow'] as mbti4}
 	<label>
 		<input
@@ -67,6 +90,9 @@
 		{mbti4}
 	</label>
 {/each}
+</form>
+
+
 
 <style>
     label{
