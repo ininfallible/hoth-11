6f901627cf1151f986bc72beb247c12c56c87d65
DaveyNavy <davidyzhu2@gmail.com>
2024-03-03 20:13:49 -0800

    Final touches

--- hoth-11-frontend/src/routes/results/+page.svelte
+++ hoth-11-frontend/src/routes/results/+page.svelte
@@ -1,11 +1,15 @@
 <script>
-  import { formData } from '../stores.js';
+  import { formData} from '../stores.js';
   import { onMount } from 'svelte';
-
+  import {get} from 'svelte/store';
 
 	import { initializeApp } from "firebase/app";
-	import { collection, getFirestore, doc, setDoc, getDocs } from "firebase/firestore";
+	import { collection, getFirestore, doc, setDoc, getDocs  } from "firebase/firestore";
 	import { query, where } from "firebase/firestore";
+  const descriptions = {dirt: "Shower Frequency", mbti1: "Introversion/Extroversion", mbti4: "Spontaneous/Longterm Planning",
+  mbti3: "Feeings/Logic", mbti2: "Present/Future Focus", diff: "Major difficulty", like: "Major satisfaction", 
+  dorm: "Time in dorm", soc: "Preferred Social Media", intr: "Cultured in the Internet", pet: "Preferred pet",
+  music: "Favorite music genre", food: "Favorite Hill food", caf: "Caffeine source", typ: "Typing speed", major: "Major" };
 
 	const firebaseConfig = {
 	apiKey: "AIzaSyDfECRJ5a6BRZS2ut9gCG061WinluJOHcI",
@@ -23,43 +27,56 @@
 
   // let data = {foo:"bar", baz: "qux"};
   let data = {};
-  let items = [];
+  var items = [];
 
   formData.subscribe((value) => {
     data = value;
   }); 
 
-  // onMount(() => {
-  //   data = get(formData)
-  // });
+  let myData = get(formData);
+  let majorToQuery = data.major;
+
+
     async function getData () {
       const dbRef = collection(database, "formResults");
-      const q = query(dbRef, where("major", "==", "Poopy Rachel"));
+      const q = query(dbRef, where("major", "==", majorToQuery));
       console.log(q);
 
       const querySnapshot = await getDocs(q);
+
+      var counts = {};
       querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+        data = doc.data();
+        console.log("myData", myData);
+        for (var category in myData)
+        {
+          if (!(category === undefined) && myData[category] && data[category])
+          {
+            if (myData[category] == data[category])
+            {
+              counts[category] = (counts[category]+1) || 1 ;
+            }
+          }
+        }
       });
+
+      let total = counts["major"];
+      $: for (let d in data)
+      {
+        items = [...items];
+        if (!counts[d]) { counts[d] = 0; }
+        items.push({"key": descriptions[d] ,"value": (counts[d] * 100 / total).toFixed(2)});
+      }
     }
-
-  $: for (let d in data)
-  {
-    console.log("Updating items based on data");
-    if (data.hasOwnProperty(d))
-      items.push({"key": d ,"value": data[d]});
-  }
-
 getData();
-
 </script>
 
-<!-- <p>{data.foo}</p> -->
+<h1>The following percentage of other students in your major shared the same opinion as you!</h1>
+
+
 {#each items as item}
   <div>
     <h2>{item.key}</h2>
-    <p>{item.value}</p>
+    <p>{item.value} %</p>
   </div>
 {/each}
-
